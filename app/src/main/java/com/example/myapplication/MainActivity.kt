b2d5a4d497f8d30eae294739b9fa4ec8951e18d9
JeremiJD <jeremidjeremi@gmail.com>
2021-11-18 13:29:39 +0100

    kurwa

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.wyswietlanie).setOnClickListener {
+            findViewById<EditText>(R.id.Tekscik).setText("Jeremi \n Damski")
+            findViewById<TextView>(R.id.innytekst).setText(R.string.tekscik)
+        }
     }
 }
